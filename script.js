e62686bf5ac0129b3dad08bfb8d66f34f0cd8289
sayak25-b <bhattacharyasayak977@gmail.com>
2026-06-02 19:28:26 +0530

    Updated protfolio..

--- script.js
+++ script.js
@@ -144,9 +144,10 @@ function typeEffect() {
         });
 
         // ==================== API Configuration ====================
-            const API_BASE_URL = window.location.protocol === 'file:'
-                ? 'https://sayak-protfolio.onrender.com/api'
-                : '/api';
+            // const API_BASE_URL = window.location.protocol === 'file:'
+            //     ? 'https://sayak-protfolio.onrender.com/api'
+            //     : '/api';
+            const API_BASE_URL = 'https://sayak-protfolio.onrender.com/api';
 
         // ==================== Form Validation and Submission ====================
         const contactForm = document.getElementById('contactForm');
